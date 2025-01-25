7fce6d3a1f11e95d3bb945e034924bc0d26135bb
Sanjay Bisht <johndoe@example.com>
2025-01-25 21:07:38 +0530

    First Commit

---
+++ src/main/java/com/Incident/Management/IncidentManagementApplication.java
@@ -0,0 +1,15 @@
+package com.Incident.Management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncidentManagementApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(IncidentManagementApplication.class, args);
+
+	}
+
+}
